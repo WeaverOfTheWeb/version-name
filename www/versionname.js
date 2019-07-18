08883c61a102a1f140f7b2f9dfbebfb043be7bc3
Lewis <1872377+WeaverOfTheWeb@users.noreply.github.com>
2019-07-18 22:22:29 +0100

    sdf

--- www/versionname.js
+++ www/versionname.js
@@ -1,9 +1,7 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-
-    getVersionName: function (success, error) {
-        exec(success, error, 'VersionName', 'getVersionName', []);
+    getVersionName: function (packageName, success, error) {
+        exec(success, error, 'VersionName', 'getVersionName', [packageName]);
     }
-
 };
